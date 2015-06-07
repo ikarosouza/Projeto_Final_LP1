a0bfa0c9a2bb0c866f5b6ec440cd2f7257b6db01
IKARO <ikarolp@hotmail.com>
2015-06-07 16:05:09 -0300

    Cria o jogo

--- main.cpp
+++ main.cpp
@@ -1,56 +1,87 @@
-//g++ main.cpp -o main $(pkg-config --cflags --libs allegro-5.0 allegro_native_dialog allegro_primitives allegro_ttf allegro_font)
-
 #include <iostream>
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_native_dialog.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_font.h>
-#include "option.h"
+#include "map.h"
+#include "player.h"
 
-#define LARGURA 800
-#define ALTURA 600
+#define WIDTH 1000
+#define HEIGHT 700
 
-int main() {
+using namespace std;
 
-  ALLEGRO_DISPLAY *display = NULL;
-  ALLEGRO_FONT *font = NULL;
+int main(){
+  ALLEGRO_DISPLAY *display;
 
-  al_init();
-  al_init_font_addon();
-  al_init_ttf_addon();
+  const float FPS = 60.0;
+  bool end = false;
+  int mouse_x = 0, mouse_y = 0;
 
-  Option *option = new Option("Novo Jogo");
+  if (!al_init()) {
+    cout << "Erro ao inicializar allegro" << endl;
+    return -1;    
+  }
 
   al_set_new_display_flags(ALLEGRO_WINDOWED);
 
-  display = al_create_display(LARGURA, ALTURA);
-  font = al_load_font("primetime.ttf", 24, 0);
-  al_clear_to_color(al_map_rgb(255, 255, 255));
+  display = al_create_display(WIDTH, HEIGHT);
 
-  if (!display) {
-    al_show_native_message_box(display, "Olá", "Opções do display", "Mensage,", NULL, 0);
-  }
+  al_set_window_title(display, "Teste");
 
-  al_set_window_title(display, "Delicia");
-  option->draw();
+  al_install_keyboard();
+  al_install_mouse();
 
+  Map *map = new Map(WIDTH, HEIGHT);
+  map->draw();
+  Player *player = new Player(WIDTH - 600, HEIGHT - 279);
+  player->draw();
   al_flip_display();
 
-  bool finalizado = false;
+  
+  ALLEGRO_MOUSE_STATE mouse;
   ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
   al_register_event_source(event_queue, al_get_display_event_source(display));
+  al_register_event_source(event_queue, al_get_keyboard_event_source());
+  al_register_event_source(event_queue, al_get_mouse_event_source());
 
-    while (!finalizado) {
+  
+
+  while (!end) {
       ALLEGRO_EVENT events;
       al_wait_for_event(event_queue, &events);
 
       if(events.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
-        finalizado = true;
+        break;
       }
+
+      if(events.keyboard.keycode == ALLEGRO_KEY_DOWN){
+        player->getDown();
+      }
+
+      if(events.keyboard.keycode == ALLEGRO_KEY_UP){
+        player->getUp();
+      }
+
+      if(events.type == ALLEGRO_EVENT_MOUSE_AXES) {
+        al_get_mouse_state(&mouse);
+        cout << "x: " << mouse.x << " y:" << mouse.y << endl;
+      }
+
+      if(al_mouse_button_down(&mouse, 1)){ 
+        player->shoot();
+      }
+
+      if((mouse_x != mouse.x) || (mouse_y != mouse.y)){
+        mouse_x = mouse.x;
+        mouse_y = mouse.y; 
+      }
+
+      map->draw();
+      player->draw();
+      player->drawScope(mouse_x, mouse_y);
+      al_flip_display(); 
     }
+
     al_destroy_display(display);
-    al_destroy_font(font);
+    al_destroy_event_queue(event_queue);
 
   return 0;
 }
